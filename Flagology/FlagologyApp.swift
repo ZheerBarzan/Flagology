fe707fce2158807748705e4ef82bf65688fd073a
zheer barzan <zheerbarzan85@outlook.com>
2024-12-30 18:30:42 +0300

    Initial Commit

---
+++ Flagology/FlagologyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlagologyApp.swift
+//  Flagology
+//
+//  Created by Zheer Barzan on 30/12/24.
+//
+
+import SwiftUI
+
+@main
+struct FlagologyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
